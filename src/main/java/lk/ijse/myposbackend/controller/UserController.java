16abb5a7758ce4eaec8d6f1b48914acef43f6e72
Charutha-Herath <charuh2023@gmail.com>
2024-08-25 01:29:51 +0530

    updated user controller

--- src/main/java/lk/ijse/myposbackend/controller/UserController.java
+++ src/main/java/lk/ijse/myposbackend/controller/UserController.java
@@ -30,24 +30,52 @@ public class UserController extends HttpServlet {
             InitialContext ctx = new InitialContext();
             DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/pos");
             this.connection=pool.getConnection();
+            System.out.println("connection run..");
         } catch (NamingException | SQLException e) {
             throw new RuntimeException(e);
         }
     }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        System.out.println("Trigger doGet Method");
+        String action = req.getParameter("action");
+
+        if (action.equals("checkUser")){
+            String userName = req.getParameter("userName");
+            System.out.println("doGet username : "+userName);
+            checkUser(req,resp,userName);
+        }
+    }
+
+    protected void checkUser(HttpServletRequest req, HttpServletResponse resp, String userName){
+
+        System.out.println("check user method");
+        UserDb userDb = new UserDb();
+        UserDTO user = userDb.getUser(userName, connection);
+        System.out.println("done password : "+user.getPassword());
+        Jsonb jsonb = JsonbBuilder.create();
+
+        try {
+            var json = jsonb.toJson(user);
+            resp.setContentType("application/json");
+            resp.getWriter().write(json);
+        } catch (IOException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("Trigger doPost Method");
         if (req.getContentType()!=null && req.getContentType().toLowerCase().startsWith("application/json")){
             Jsonb jsonb = JsonbBuilder.create();
 
             UserDTO userDTO = jsonb.fromJson(req.getReader(),UserDTO.class);
 
-            System.out.println(userDTO.getUserName());
-            var userDb = new UserDb();
+            System.out.println(userDTO.getUsername());
+            /*var userDb = new UserDb();
             boolean result = userDb.saveUser(userDTO, connection);
 
             if (result){
@@ -55,7 +83,7 @@ public class UserController extends HttpServlet {
                 resp.getWriter().write("User information saved successfully!");
             }else {
                 resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Fail to saved user information!");
-            }
+            }*/
 
         }
     }
