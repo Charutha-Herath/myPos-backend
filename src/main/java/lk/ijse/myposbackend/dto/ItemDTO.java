c25417c10e64a52a44e79c0e60494f988f4ef4bd
Charutha-Herath <charuh2023@gmail.com>
2024-08-26 00:39:53 +0530

    updated itemDb

---
+++ src/main/java/lk/ijse/myposbackend/dto/ItemDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.myposbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDTO implements Serializable {
+    private String itemCode;
+    private String description;
+    private String qty;
+    private String price;
+}
